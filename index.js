433aeaad325540e8f16b9d6796e31c9989690efb
Terry Moore <Terry.Moore@dudesolutions.com>
2016-01-10 15:31:50 -0500

    Fix toggle call :: master

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 'use strict';
 
 const Hapi = require('hapi');
+const garage = require('../lib/garage');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
-    port: 8000
+  host: 'localhost',
+  port: 8000
 });
 
 // Add the route
 server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: function (request, reply) {
-        return reply('hello world');
-    }
+  method: 'GET',
+  path: '/hello',
+  handler: function(request, reply) {
+    return reply('hello world');
+  }
 });
 
+server.route({
+    method: 'GET',
+    path: '/status',
+    handler: function(request, reply) {
+      return garage.status(function(response) {
+          reply(JSON.stringify({
+              status: response
+            });
+          });
+      }
+    });
 
+  server.route({
+      method: 'POST',
+      path: '/toggle',
+      handler: function(request, reply) {
+        return garage.toggle(function(response) {
+            reply(JSON.stringify({
+                status: response
+              });
+            })
+        });
+    }
+  });
+
+server.route({
+  method: 'POST',
+  path: '/toggle',
+  handler: function(request, reply) {
+    return garage.log(function(response) {
+      reply(response)
+    })
+  });
+}
+});
 
 
 
 // Start the server
 server.start((err) => {
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
 });
 
 
